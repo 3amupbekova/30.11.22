e7b005e770621b42e2ecf91f283ed338b2c34985
Namat Bayamanov <namataxvd15@gmail.com>
2023-02-13 21:24:59 +0600

    made two sections and styled them

---
+++ 07.02.23/index.js
@@ -0,0 +1,19 @@
+function sumAB (a,b) {return a+b}
+var textLenght=text.lenght;
+var text = "Karakol";
+var i=5;
+while (i<25);
+
+
+
+
+let example1=[];
+let example2=[3, 4, 1, 54, 33]; это массив из 5 элементов
+let example3=["hello",55,true,null];
+
+let example4=[ "Aijan", "Dastan", "omurbek"];
+
+let example5=["Aidarken", "Baetov", "Balyckhy", "Batken", "Bishkek", "Jalal-Abad", "Isfana", "Kadamjay", "Kaindy", "Kant", "Karakol",];
+
+console.log(example5);
+
